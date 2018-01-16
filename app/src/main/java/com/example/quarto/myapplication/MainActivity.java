19c805c365baf7d2231755a4ac3c509d0e6587a5
Silas Oliveira <silasadailtonn@gmail.com>
2018-01-15 22:42:50 -0200

    fragment

--- app/src/main/java/com/example/quarto/myapplication/MainActivity.java
+++ app/src/main/java/com/example/quarto/myapplication/MainActivity.java
@@ -1,16 +1,90 @@
 package com.example.quarto.myapplication;
 
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String STATE_FRAGMENT = "state_of_fragment";
+    private Button mButton;
+    private boolean isFragmentDisplayed = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mButton = findViewById(R.id.button_open_fragment);
+        mButton.setOnClickListener(new buttonOpenFragment());
 
+        if (savedInstanceState != null) {
+            isFragmentDisplayed = savedInstanceState.getBoolean(STATE_FRAGMENT);
+
+            if (isFragmentDisplayed) {
+                mButton.setText(R.string.close);
+            }
+        }
+
+
+    }
+
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        savedInstanceState.putBoolean(STATE_FRAGMENT, isFragmentDisplayed);
+
+        super.onSaveInstanceState(savedInstanceState);
+    }
+
+    public void displayFragment() {
+        SimpleFragment simpleFragment = SimpleFragment.newInstance();
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager
+                .beginTransaction();
+
+        fragmentTransaction.add(R.id.fragment_container, simpleFragment)
+                .addToBackStack(null)
+                .commit();
+
+        mButton.setText(R.string.close);
+
+        isFragmentDisplayed = true;
+    }
+
+    public void closeFragment() {
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        SimpleFragment simpleFragment = (SimpleFragment) fragmentManager
+                .findFragmentById(R.id.fragment_container);
+
+        if (simpleFragment != null) {
+
+            FragmentTransaction fragmentTransaction = fragmentManager
+                    .beginTransaction();
+
+            fragmentTransaction.remove(simpleFragment).commit();
+        }
+
+        mButton.setText(R.string.open);
+
+        isFragmentDisplayed = false;
+
+
+    }
+
+    private class buttonOpenFragment implements View.OnClickListener {
+
+        @Override
+        public void onClick(View view) {
+            if (!isFragmentDisplayed) {
+                displayFragment();
+            }else {
+                closeFragment();
+            }
+        }
     }
 
 }
--- app/src/main/java/com/example/quarto/myapplication/SimpleFragment.java
+++ app/src/main/java/com/example/quarto/myapplication/SimpleFragment.java
@@ -2,14 +2,18 @@ package com.example.quarto.myapplication;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.RadioGroup;
+import android.widget.RatingBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class SimpleFragment extends Fragment {
+public class SimpleFragment extends android.support.v4.app.Fragment {
     private static final int YES = 0;
     private static final int NO = 1;
 
@@ -18,18 +22,21 @@ public class SimpleFragment extends Fragment {
 
     @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         final View rootView = inflater.inflate(R.layout.fragment_simple,container, false);
 
         final RadioGroup radioGroup = (RadioGroup) rootView.findViewById(R.id.radio_group);
+        final RatingBar ratingBar = (RatingBar) rootView.findViewById(R.id.ratingbar);
 
         radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup group, int checkedId) {
 
+
                 View radioButton = radioGroup.findViewById(checkedId);
 
                 int index = radioGroup.indexOfChild(radioButton);
+                Log.d("teste", "onCheckedChanged: "+index);
 
                 TextView textView = (TextView) rootView.findViewById(R.id.fragment_header);
 
@@ -49,6 +56,19 @@ public class SimpleFragment extends Fragment {
             }
         });
 
+        ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float v, boolean b) {
+                Toast.makeText(getActivity(), "Minhas estrelas: "+v, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
         return rootView;
+    }
+
+    public static SimpleFragment newInstance() {
+
+        return new SimpleFragment();
     }
 }
